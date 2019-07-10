bcb7d42977b0114796881e6fae387b6e19922c27
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-10 20:12:03 +0300

    In this challenge we are making models and registering thme into the admin site

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	Name = models.CharField(max_length=120)
+	Description = models.TextField()
+	Opening_time = models.TimeField
+	Closing_time = models.TimeField
+	"""docstring for ClassName"""
